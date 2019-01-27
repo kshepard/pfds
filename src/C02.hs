e830a40660aab28c1edace45bc0110460e2d22e7
Kenny Shepard <kshepard@gmail.com>
2019-01-27 18:51:13 -0500

    Add implementation of Set/UnbalancedSet

--- src/C02.hs
+++ src/C02.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# Language TypeApplications #-}
 
 module C02 where
@@ -10,5 +12,36 @@ suffixes :: [a] -> [[a]]
 suffixes []         = [[]]
 suffixes xs@(_ : t) = xs : suffixes t
 
+-- Implementation of Set/UnbalancedSet
+
+class Set s a where
+  empty  :: s a
+  insert :: a -> s a -> s a
+  member :: a -> s a -> Bool
+
+data UnbalancedSet a = E | T (UnbalancedSet a) a (UnbalancedSet a)
+  deriving (Show)
+
+instance Ord a => Set UnbalancedSet a where
+  empty = E
+
+  member _ E = False
+  member x (T a y b)
+    | x < y     = member x a
+    | x > y     = member x b
+    | otherwise = True
+
+  insert x E = T E x E
+  insert x s@(T a y b)
+    | x < y     = T (insert x a) y b
+    | x > y     = T a y (insert x b)
+    | otherwise = s
+
 main :: IO ()
-main = print $ suffixes @Int [1, 2, 3, 4]
+main = do
+  print $ suffixes @Int [1, 2, 3, 4]
+
+  let s = insert "sit" $ insert "dolor" $ insert "ipsum" $ insert "lorem" E
+  print s
+  print $ member "dolor" s
+  print $ member "should_not_exist" s
